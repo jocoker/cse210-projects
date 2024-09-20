ad271347adf2f8d0f9a0ee695972553bcb0d1ebf
Jordan C <coker.jordan@gmail.com>
2024-09-19 23:18:41 -0400

    updates

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -5,5 +5,10 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Sandbox World!");
+        Console.WriteLine("This is in C#.");
+
+        Console.Write("What is your fav color? ");
+        string color = Console.ReadLine();
+        Console.WriteLine($"I like the color {color} too!");
     }
 }
