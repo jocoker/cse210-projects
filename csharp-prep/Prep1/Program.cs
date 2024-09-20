6ec47c4d4ccd920f95d88ba44ad7cb865fd83cef
Jordan C <coker.jordan@gmail.com>
2024-09-19 23:20:23 -0400

    added comment

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,9 +4,10 @@ class Program
 {
     static void Main(string[] args)
     {
+        //Ask the user for their name.
         Console.Write("What is your first name? ");
         string fname = Console.ReadLine();
-        
+
         Console.Write("What is your last name? ");
         string lname = Console.ReadLine();
         
