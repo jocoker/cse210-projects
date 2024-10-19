b84eb19e60598e88ea7a66e9e1845aceeeff7101
Jordan C <coker.jordan@gmail.com>
2024-10-19 13:09:36 -0400

    finished

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,17 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        // Create a base "Assignment" object
+        Assignment a1 = new Assignment("Jordan C", "Programming Basics");
+        Console.WriteLine(a1.GetSummary());
+
+        // Now create the derived class assignments
+        MathAssignment a2 = new MathAssignment("John Smith", "Math", "10.1", "1-10");
+        Console.WriteLine(a2.GetSummary());
+        Console.WriteLine(a2.GetHomeworkList());
+
+        WritingAssignment a3 = new WritingAssignment("Jane Doe", "Economics", "The Economics of World War I");
+        Console.WriteLine(a3.GetSummary());
+        Console.WriteLine(a3.GetWritingInformation());
     }
 }
